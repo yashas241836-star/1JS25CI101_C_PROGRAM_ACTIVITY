53879180a8e75bfae56a08e3a64a8e5f800aa690
yashas241836-star <yashas241836@gmail.com>
2026-05-16 12:10:07 +0530

    Create Skilllab

---
+++ Skilllab1.c
@@ -0,0 +1,14 @@
+#include &lt;stdio.h&gt;
+int main()
+{
+printf("Name: Yashas V\n");
+printf("USN: 1JS25CI101\n");
+printf("Program: Simple Addition of Two Numbers\n");
+printf("----------------------------------------\n");
+int a, b, sum;
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+sum = a + b;
+printf("Sum = %d\n", sum);
+return 0;
+}
